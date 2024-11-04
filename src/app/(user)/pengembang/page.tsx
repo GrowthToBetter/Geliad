7e5d710c4faa9f5c42782e39f4288f017252f528
GrowthToBetter <baratrahjaga@gmail.com>
2024-11-04 07:49:46 +0700

    ,

--- src/app/(user)/pengembang/page.tsx
+++ src/app/(user)/pengembang/page.tsx
@@ -4,7 +4,7 @@ export default function Pengembang() {
   const developers = [
     { job: "General Manager", name: "Muhammad Chusni Agus, M.Pd., Gr." },
     { job: "Project Manager and Fullstack Developer", name: "Jean Richnerd Rantabaratrahjaga" },
-    { job: "Software Architect and Data Scientist", name: "Shabri Sebastian Siregar" },
+    { job: "Software Architect and Data Ai Engineer", name: "Shabri Sebastian Siregar" },
     { job: "Human Resource and Public Relation ", name: "Syafakarim Akbar" },
   ];
 
@@ -12,7 +12,7 @@ export default function Pengembang() {
     <div className="flex flex-col min-h-screen-minus-10">
       <div className="flex-grow mt-[100px] p-10 max-w-5xl mx-auto">
         <div className="mb-10">
-          <h1 className="text-[29px] font-medium text-[#F45846] mb-5">Developer Credit</h1>
+          <h1 className="text-[29px] font-medium text-Secondary mb-5">Developer Credit</h1>
           <div className="grid grid-cols-2 gap-y-2 gap-x-16 max-w-3xl">
             {developers.map((developer, index) => (
               <React.Fragment key={index}>
